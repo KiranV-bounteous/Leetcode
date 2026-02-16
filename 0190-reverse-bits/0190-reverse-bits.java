92773afcc6fdc1b6553b7c3b144fc41042d77a2b
KiranV-bounteous <kiran.v@bounteous.com>
2026-02-16 01:20:27 -0800

    Time: 1 ms (60.34%), Space: 42.2 MB (93.42%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,5 +1,13 @@
 public class Solution {
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;          
+            result |= (n & 1);     
+            n >>>= 1;              
+        }
+        
+        return result;
     }
 }
