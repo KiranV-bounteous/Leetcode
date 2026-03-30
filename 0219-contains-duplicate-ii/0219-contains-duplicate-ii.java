afdb0664fb527c4aa7221ff2ef0fdab9672b8d5c
KiranV-bounteous <kiran.v@bounteous.com>
2026-03-29 22:43:22 -0700

    Time: 105 ms (9.97%), Space: 74.8 MB (90.18%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        int n=nums.length;
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=i+1;j<n&&j<=i+k;j++)
-            {
-                if(nums[i]==nums[j]&&Math.abs(i-j)<=k)
-                {
-                    return true;
-                }
+        Set<Integer> set = new TreeSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
             }
+            set.add(nums[i]);
+            if(set.size() > k) {
+                set.remove(nums[i-k]);
+            }
+
+
         }
-        System.gc();
-        return false;
+        
+        
+
+     return false;
     }
 }
